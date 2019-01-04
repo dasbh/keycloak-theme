75b4b738dc488d095134971e8caf763d5e8fe35d
Bhagavan Das <bd@trivand.com>
2019-01-04 00:16:38 +0000

    Custom SPI

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # keycloak-theme
 Custom theme for Keycloak
+
+### Enable auth for WildFly app server hosting keycloak, so that we can deploy our custom extensions using gradle plugin
+
+```
+$ cd  /opt/jboss/keycloak/bin
+$ ./add-user.sh -u admin -p admin -r ManagementRealm
+$ ./jboss-cli.sh --connect --controller=localhost:10090
+[standalone@localhost:10090 /] security enable-http-auth-management
+
+```
+
---
+++ keycloak-custom-spi/magic-link/src/main/java/org/keycloak/custom/magic/MagicLinkFormAuthenticatorFactory.java
@@ -0,0 +1,84 @@
+package org.keycloak.custom.magic;
+
+
+import org.keycloak.Config;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+
+import java.util.List;
+
+/**
+ * @author <a href="mailto:sthorger@redhat.com">Stian Thorgersen</a>
+ */
+public class MagicLinkFormAuthenticatorFactory implements AuthenticatorFactory {
+
+    public static final String ID = "magic-form";
+
+    private static final AuthenticationExecutionModel.Requirement[] REQUIREMENT_CHOICES = {
+            AuthenticationExecutionModel.Requirement.REQUIRED,
+            AuthenticationExecutionModel.Requirement.OPTIONAL,
+            AuthenticationExecutionModel.Requirement.DISABLED
+    };
+
+    @Override
+    public Authenticator create(KeycloakSession session) {
+        return new MagicLinkFormAuthenticator();
+    }
+
+    @Override
+    public String getId() {
+        return ID;
+    }
+
+    @Override
+    public String getReferenceCategory() {
+        return "magic";
+    }
+
+    @Override
+    public boolean isConfigurable() {
+        return false;
+    }
+
+    @Override
+    public boolean isUserSetupAllowed() {
+        return true;
+    }
+
+    @Override
+    public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+        return REQUIREMENT_CHOICES;
+    }
+
+    @Override
+    public String getDisplayType() {
+        return "Magic Link";
+    }
+
+    @Override
+    public String getHelpText() {
+        return "Magic Link";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return null;
+    }
+
+    @Override
+    public void init(Config.Scope config) {
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory factory) {
+    }
+
+    @Override
+    public void close() {
+    }
+
+}
